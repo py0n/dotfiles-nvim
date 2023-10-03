8830a4de9dde21e548e63ed2e8c0b32db90c84f8
py0n <py0n@users.noreply.github.com>
2023-10-03 10:12:47 +0900

    modify: context_filetype, vim_preciousをpacker管理下に移動する

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,10 +26,12 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     -- My plugins here
+    use 'Shougo/context_filetype.vim'
     use {
         'notjedi/nvim-rooter.lua',
         config = function() require'nvim-rooter'.setup() end
     }
+    use 'osyo-manga/vim-precious'
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
