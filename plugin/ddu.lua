b539eb2f16cd9a5b4f20ae8f7f359528442271ec
py0n <py0n@users.noreply.github.com>
2023-10-04 11:50:15 +0900

    modify: luaで書き換える

--- plugin/ddu.lua
+++ plugin/ddu.lua
@@ -46,21 +46,6 @@ call ddu#custom#patch_global({
 \   },
 \ })
 
-function! s:ddu_my_settings() abort
-    nnoremap <buffer><silent> <CR>    <Cmd>call ddu#ui#ff#do_action('itemAction')<CR>
-    nnoremap <buffer><silent> <Space> <Cmd>call ddu#ui#ff#do_action('toggleSelectItem')<CR>
-    nnoremap <buffer><silent> i       <Cmd>call ddu#ui#ff#do_action('openFilterWindow')<CR>
-    nnoremap <buffer><silent> q       <Cmd>call ddu#ui#ff#do_action('quit')<CR>
-endfunction
-autocmd FileType ddu-ff call s:ddu_my_settings()
-
-function! s:ddu_filter_my_settings() abort
-    inoremap <buffer><silent> <CR> <Esc><Cmd>close<CR>
-    nnoremap <buffer><silent> <CR> <Cmd>close<CR>
-    nnoremap <buffer><silent> q    <Cmd>close<CR>
-endfunction
-autocmd FileType ddu-ff-filter call s:ddu_filter_my_settings()
-
 nnoremap [denite] <Nop>
 nmap     <Space>u [denite]
 
@@ -73,3 +58,24 @@ nnoremap <silent> [denite]m <Cmd>call ddu#start({'sources': [{'name':'register'}
 " 最近使用したファイル
 nnoremap <silent> [denite]p <Cmd>call ddu#start({'sources': [{'name':'mr'}]})<CR>
 ]])
+
+vim.api.nvim_create_augroup('plugin/ddu', {clear = true})
+vim.api.nvim_create_autocmd('FileType', {
+    group    = 'plugin/ddu',
+    pattern  = 'ddu-ff',
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', '<Cmd>call ddu#ui#ff#do_action("itemAction")<CR>', {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(0, 'n', ' ', '<Cmd>call ddu#ui#ff#do_action("toggleSelectItem")<CR>', {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(0, 'n', 'i', '<Cmd>call ddu#ui#ff#do_action("openFilterAction")<CR>', {noremap = true, silent= true})
+        vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<Cmd>call ddu#ui#ff#do_action("quit")<CR>', {noremap = true, silent = true})
+    end
+})
+vim.api.nvim_create_autocmd('FileType', {
+    group    = 'plugin/ddu',
+    pattern  = 'ddu-ff-filter',
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'i', '<CR>', '<Esc><Cmd>close<CR>', {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', '<Cmd>close<CR>', {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<Cmd>close<CR>', {noremap = true, silent = true})
+    end
+})
