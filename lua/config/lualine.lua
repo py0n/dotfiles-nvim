047cbfafa0c379bebe715b23357ef223c6dcaff1
py0n <py0n@users.noreply.github.com>
2025-03-07 11:20:53 +0900

    modify: デフォルト値の差分のみ

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -41,43 +41,14 @@ local function charCodeUnderCursor()
     return vim.fn.MyCharCode()
 end
 
+-- https://github.com/nvim-lualine/lualine.nvim
+-- デフォルト値の差分のみ記述
 require('lualine').setup {
     options = {
-        icons_enabled = false,
-        theme = 'auto',
         component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
+        section_separators   = { left = '', right = ''},
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
         lualine_x = {charCodeUnderCursor, 'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
 }
